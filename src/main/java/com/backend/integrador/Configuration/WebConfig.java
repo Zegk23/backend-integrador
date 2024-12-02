6f231a512a3c38ff873f1996c4b6c9c55c1ddc58
Luis <lmartinezquijandria@gmail.com>
2024-12-02 11:55:27 -0500

    Mejora de logs

--- src/main/java/com/backend/integrador/Configuration/WebConfig.java
+++ src/main/java/com/backend/integrador/Configuration/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**") // Aplica CORS solo a rutas que empiezan con /api
                 .allowedOrigins(
                         "http://localhost:3000", // Origen para desarrollo
-                        "https://front-integrador-velazco.vercel.app/" // Origen para producción (actualízalo cuando subas el frontend)
+                        "https://front-integrador-velazco.vercel.app/",
+                        "https://velazco-pagina.vercel.app/" // Origen para producción (actualízalo cuando subas el frontend)
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
                 .allowedHeaders("*") // Permitir todos los encabezados
